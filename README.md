b420dc2a39e46ec34b77eb085e57d4525fa35860
Daniel Wehner <dawehner@googlemail.com>
2017-06-22 13:23:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Contenta recipe bot
+
+One major advantage of a decoupled approach is to have a rich data model, which
+can be accessed from more places.
+
+These days a really hiped feature are chatbots. This project provides one example
+implementation using (botmaster.ai)[http://botmasterai.com/documentation/latest/].
+Please experiment with other tools, and please join the #contenta slack 
+
+## Setup
+
+* Set up .env variables
+* Start the bot using ```node bot.js```
+* Try to interact with the bot
+
+## Interaction
+
+* What should I cook?
+* What are the ingredients?
+* Show me the instructions!
+
+## Features
+
+* Twitter support
+* More integrations to come
+* Commands: Get a random recipe
---
+++ bot.js
@@ -0,0 +1,63 @@
+const Botmaster = require('botmaster');
+const axios = require('axios');
+const _ = require('lodash');
+
+require('dotenv').config();
+
+const botmaster = new Botmaster();
+const SessionWare = require('botmaster-session-ware');
+
+// @todo Add more clients later!
+const TwitterBot = require('botmaster-twitter-dm');
+
+const twitterSettings = {
+  credentials: {
+    consumerKey: process.env.TWITTER__CONSUMERKEY,
+    consumerSecret: process.env.TWITTER__CONSUMERSECRET,
+    accessToken: process.env.TWITTER__ACCESSTOKEN,
+    accessTokenSecret: process.env.TWITTER__ACCESSTOKENSECRET,
+  }
+};
+botmaster.addBot(new TwitterBot(twitterSettings));
+
+const sessionWare = SessionWare();
+botmaster.useWrapped(sessionWare.incoming, sessionWare.outgoing);
+
+
+const fetchRandomRecipe = () => {
+  return axios.get('http://127.0.0.1:8888/api/recipes?page[limit]=20')
+    .then(res => res.data.data)
+    .then(entries => _.map(entries, entry => entry.attributes))
+    .then(recipes => recipes[_.random(0, recipes.length - 1)])
+};
+
+
+botmaster.use({
+  type: 'incoming',
+  name: 'my-example-recipe',
+  controller: (bot, update) => {
+    // Help with knowing what to cook.
+    if (update.message.text.indexOf('What') !== -1 && update.message.text.indexOf('cook') !== -1) {
+      fetchRandomRecipe()
+        .then(recipe => {
+          update.session.recipe = recipe;
+          bot.reply(update, recipe.title)
+            .catch(console.error);
+        })
+        .catch(console.error);
+    }
+    // Show the ingredients
+    if (update.message.text.indexOf('ingredients') !== -1) {
+      update.session.recipe.ingredients.forEach((ingredient) => {
+        bot.reply(update, ingredient)
+          .catch(console.error);
+      });
+    }
+    // Show the instructions
+    if (update.message.text.indexOf('instructions') !== -1) {
+      bot.reply(update, update.session.recipe.instructions)
+        .catch(console.error);
+    }
+  }
+});
+
